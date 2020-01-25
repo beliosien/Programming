c75cacfea6f43a0c8268850e018e5d51dfc74276
philippe <francois-philippe.ossim-belias@polymtl.ca>
2020-01-25 15:08:58 -0500

    creation of the cube class and implementation of the movement of the cube in progress

---
+++ ChasingCube/Assets/Scripts/CubeScript.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+///  Class cube the base of all type of cube in the game 
+/// </summary>
+public class CubeScript
+{
+    private float damagePt;
+    private float health;  
+    private float speed;
+    GameConstants.CubeType cubeType; 
+
+    #region  constructor
+
+    ///<summary>
+    /// constructor 
+    ///</summary>
+    CubeScript(float damagePt, float health, GameConstants.CubeType cubeType)
+    {
+        this.damagePt = damagePt;
+        this.health = health;
+        this.speed = 10.0f;
+        this.cubeType = cubeType;
+    }
+
+    #endregion
+
+    #region properties
+
+    ///<summary>
+    /// Get and Set the damagePt of the cube
+    ///</summary>
+    public float DamagePt
+    {
+        get { return damagePt; } 
+        set { damagePt = value; }
+    }
+
+    ///<summary>
+    /// Get the health of the cube
+    ///</summary>
+    public float Health
+    {
+        get {return health;}
+    }
+
+    ///<summary>
+    /// Get the speed of the cube
+    ///</summary>
+    public float Speed
+    {
+        get {return speed;}
+    }
+
+    ///<summary>
+    /// Get the cube type
+    ///</summary>
+    public GameConstants.CubeType CubeType
+    {
+        get {return cubeType;}
+    }
+
+    #endregion
+
+    #region Public methods
+
+    ///<summary>
+    /// move the cube in the direction that we want
+    ///</summary>
+    public void Move()
+    {
+        float moveHorizontal = -1*Input.GetAxis("Horizontal");
+        float moveVertical = -1*Input.GetAxis("Vertical");
+        //transform.Translate(moveHorizontal*Time.deltaTime*speed, 0.0f, moveVertical*Time.deltaTime*speed);
+    }
+
+    ///<summary>
+    /// the hunter chase the hunted
+    ///<param name="cube">cube we are chasing after</param>
+    ///</summary>
+    public void ChaseCube(GameObject cube) {
+        
+    }
+
+    #endregion
+ 
+}
---
+++ ChasingCube/Assets/Scripts/GameConstants.cs
@@ -0,0 +1,16 @@
+
+///<summary>
+/// Game constants
+///</summary>
+public static class GameConstants{
+    ///<summary>
+    /// the type of cube in the game
+    ///</summary>
+    public enum CubeType
+    {
+        speedsterCube = 0,
+        slowerCube = 1,
+        tricksterCube = 2,
+        jumperCube = 3
+    }
+} 
