ec49858114cf83cdb9d16ec41470a89a0cad5e87
philippe <francois-philippe.ossim-belias@polymtl.ca>
2020-03-01 00:33:46 -0500

    Timer class added

---
+++ ChasingCube/Assets/Scripts/ObstacleSpawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+///<summary>
+// This Class spawn the obstacle 
+// on the scene randomly
+///</summary>
+public class ObstacleSpawner : MonoBehaviour
+{
+    [SerializeField]
+    GameObject prefabObstacle;
+
+    // spawn control
+	Timer spawnTimer;
+    const float SpawnDelay = 0.3f;
+    const int MaxNumObstacles = 5;
+
+    // spawn location support
+    Vector3 location = Vector3.zero;
+	float minSpawnX;
+	float maxSpawnX;
+	float minSpawnZ;
+	float maxSpawnZ;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
